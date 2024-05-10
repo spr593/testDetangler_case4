52aa9cc85a41d592cdd733301ecb786ed6c144ba
spr593 <salome.perez8@gmail.com>
2024-05-10 18:10:10 -0500

    comments for future branches in main

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from model import *
+
+if __name__=="__main__":
+    print("Implement a Stack Model (SM) Learning")
+    print("Model.py will implement the SM functionality in branch b1.")
+    print("Unnittest_model.py will implement the unittest verification of output shapes and f1 scores for the model evaluation in branch b2.")

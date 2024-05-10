3319b3f34afcb742929749e9f3d6b8c592999b34
spr593 <salome.perez8@gmail.com>
2024-05-10 18:14:59 -0500

    b1: SM model preprocessing

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from sklearn.ensemble import StackingClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.impute import SimpleImputer
@@ -27,3 +28,12 @@ def train_validate(X_train, X_test, X_val, y_train):
 def test (X_test):
     test_predictions = stack_model_pipeline.predict(X_test)
     return test_predictions
+
+def preprocessing(X, y):
+    imputer = SimpleImputer(strategy='mean')  # Replace missing values with the mean of the column
+    scaler = StandardScaler()  # Scale data to have zero mean and unit variance
+    X = imputer.fit_transform(X)
+    X = scaler.fit_transform(X)
+
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.3, random_state=33)
+    X_test, X_val, y_test, y_val = train_test_split(X_temp, y_temp, test_size=0.4, random_state=33)

d1e99e0a96a545dd2027c55e614add41a2a89dd1
spr593 <salome.perez8@gmail.com>
2024-05-10 18:24:46 -0500

    b2: unit test outline for SM model

---
+++ unittest_model.py
@@ -0,0 +1,10 @@
+import unittest
+from model import *
+
+
+class TestStackingModel(unittest.TestCase):
+    def __init__(self, model):
+        self.model = model
+
+    def test_model_performance(self):
+        pass
